80adab6e23b70c1c20f1f1550ad2eb797b60c2bf
LeeYoungKyung <yklee1040@naver.com>
2023-11-05 20:57:51 +0900

    rock scissors paper

---
+++ project1/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    //이거 중요
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
